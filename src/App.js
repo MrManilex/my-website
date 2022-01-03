9cc2ee3132ee7f00aca7d8fbe666bf7aa9925f9d
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-03 18:00:42 -0500

    add basic styling

--- src/App.js
+++ src/App.js
@@ -2,31 +2,48 @@ import './App.css';
 
 function App() {
   return (
-    <main>
-      <nav>
-        <li>About Me</li>
-        <li>Proficiencies</li>
-        <li>My Projects</li>
+    <>
+      <nav className='navbar is-dark has-shadow'>
+        <div className='navbar-brand'>
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div className='navbar-menu'>
+          <div className='navbar-start'>
+            <a class="navbar-item">About Me</a>
+            <a class="navbar-item">Skills</a>
+            <a class="navbar-item">My Projects</a>
+            <a class="navbar-item">Resume</a>
+          </div>
+        </div>
       </nav>
-      <section>
-        <div className='about-me'>
-          <p>About Me!!!!!</p>
+      <main className='is-flex is-flex-direction-column is-align-items-center has-background-light'>
+        <div>
+          <h1 className='name title is-1'>Alexander Maniwang</h1>
+          <section>
+            <div className='about-me section'>
+              <p className='title is-3'>About Me!!!!!</p>
+            </div>
+          </section>
         </div>
-      </section>
-      <section>
-        <div className='skills'>
-          {/* coding skills go here */}
+        <section>
+          <div className='skills section'>
+            <p className='title is-3'>coding skills go here</p>
+          </div>
+        </section>
+        <section>
+          <div className='projects section'>
+            <p className='title is-3'>put my best projects here</p>
+          </div>
+        </section>
+        <div className='reach section'>
+          <p className='title is-3'>Reach Me Here:</p>
         </div>
-      </section>
-      <section>
-        <div className='projects'>
-          {/* put my best projects here */}
-        </div>
-      </section>
-      <div className='reach'>
-        <p>Reach Me Here:</p>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 

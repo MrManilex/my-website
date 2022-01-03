6581a626b2eba68e0e8aa2638162edc5dea0c441
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-03 14:42:11 -0500

    stub sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav>
+        <li>About Me</li>
+        <li>Proficiencies</li>
+        <li>My Projects</li>
+      </nav>
+      <section>
+        <div className='about-me'>
+          <p>About Me!!!!!</p>
+        </div>
+      </section>
+      <section>
+        <div className='skills'>
+          {/* coding skills go here */}
+        </div>
+      </section>
+      <section>
+        <div className='projects'>
+          {/* put my best projects here */}
+        </div>
+      </section>
+      <div className='reach'>
+        <p>Reach Me Here:</p>
+      </div>
+    </main>
   );
 }
 

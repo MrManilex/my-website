73d13047907c46fc14e486e507673248122462ed
UbuntuLex <alexandermaniwang99@gmail.com>
2022-05-20 18:33:39 -0400

    css rework

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ function App() {
         <div className="hero min-h-screen">
           <div className="text-center hero-content">
             <div className="max-w-md">
-              <div className='avatar w-6/12'>
+              <div className='avatar w-7/12'>
                 <img src="images/IMG_0544.jpeg" alt="Placeholder" className="mask mask-circle" />
               </div>
-              <h1 className="mb-5 text-5xl font-bold">
+              <p className="h-32 name text-5xl font-bold">
                 Alexander Maniwang
-              </h1>
+              </p>
               <p className="mb-5 text-2xl">
                 Full-Stack Developer
               </p>
             </div>
           </div>
         </div>
+
         <div className="divider w-8/12 m-auto" id='about-me'></div>
+
         <div className="text-center">
           <h2 className="text-4xl m-5">About Me</h2>
           <p className="w-8/12 m-auto">I am an innovative Software Engineer with a passion for problem-solving that drives me to create high quality applications that are instinctively user-friendly. My background in retail and restaurant settings help me better understand consumer and customer needs and helps me focus under pressure, stay organized, and receive constant feedback. I have a constant drive to sculpt and mitigate actions to create a more efficient and straightforward engagement for customers.</p>
@@ -37,105 +39,102 @@ function App() {
             </a>
           </div>
         </div>
+
         <div className="divider w-8/12 m-auto" id='portfolio'></div>
-        <div className="w-8/12 m-auto">
-          {/* GENSHIN BUILDS */}
-          <div className="card text-center shadow-2xl">
-            <figure className="px-10 pt-10">
-              <img src="images/GenshinBuilds.png" alt="Genshin Builds Landing Page" />
-              <img src="images/GenshinBuilds-2.png" alt="Genshin Builds Profile Page" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Genshin Builds</h2>
-              <p>Genshin Builds was designed to plan and create teams customizing each playable character for a better experience within the game Genshin Impact. This project was created in collaboration with two other individuals using a MERN stack format and an API.</p>
-              <div className="justify-center card-actions">
-                <a href="https://genshin-team-builder.herokuapp.com/" target='_blank' rel='noreferrer'>
-                  <button className='btn'>
-                    Go To App
-                  </button>
-                </a>
-                <a href="https://github.com/WangManLex/genshin-builds" target='_blank' rel='noreferrer'>
-                  <button className='btn'>
-                    Github
-                  </button>
-                </a>
+
+        <div className="flex-wrap w-8/12 m-auto">
+          <h2 className=" text-center text-4xl m-5">Portfolio</h2>
+          <div className="flex justify-center">
+            {/* GENSHIN BUILDS */}
+            <div className="card bg-base-100 shadow-xl m-2">
+              <figure><img src="images/GenshinBuilds-2.png" alt="Genshin Builds" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">Genshin Builds</h2>
+                <p>Genshin Builds was designed to plan and create teams customizing each playable character for a better experience within the game Genshin Impact. This project was created in collaboration with two other individuals using a <span className='btn-primary'>MERN</span> stack format and an <span className='btn-primary'>API.</span></p>
+                <div className="card-actions justify-end">
+                  <a href="https://genshin-team-builder.herokuapp.com/" target='_blank' rel='noreferrer'>
+                    <button className='btn'>
+                      Go To App
+                    </button>
+                  </a>
+                  <a href="https://github.com/WangManLex/genshin-builds" target='_blank' rel='noreferrer'>
+                    <button className='btn'>
+                      Github
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* ANIWORLD  */}
+            <div className="card bg-base-100 shadow-xl m-2">
+              <figure><img src="images/AniWorld-2.png" alt="AniWorld" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">AniWorld</h2>
+                <p>AniWorld is an anime search website where you're able to track animes if you are currently watching or have watched. <span className='btn-primary'>Google OAuth</span> is used to save profile information as well. By using the <span className='btn-primary'>API</span> called AniApi, information about each anime can retrieved and displayed in each details page.</p>
+                <div className="card-actions justify-end">
+                  <a href="https://ani-world.herokuapp.com/" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Go To App
+                    </button>
+                  </a>
+                  <a href="https://github.com/WangManLex/AniWorld" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Github
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          {/* ANIWORLD  */}
-          <div className="card text-center shadow-2xl">
-            <figure className="px-10 pt-10">
-              <img src="images/AniWorld.png" alt="AniWorld Landing Page" />
-              <img src="images/AniWorld-2.png" alt="AniWorld Search Page" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">AniWorld</h2>
-              <p>AniWorld is an anime search website where you're able to track animes if you are currently watching or have watched. Google OAuth is used to save profile information as well. By using the API AniApi, information about each anime can retrieved and displayed in each details page.</p>
-              <div className="justify-center card-actions">
-                <a href="https://ani-world.herokuapp.com/" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Go To App
-                  </button>
-                </a>
-                <a href="https://github.com/WangManLex/AniWorld" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Github
-                  </button>
-                </a>
+          <div className="flex justify-center">
+            {/* FARMING SIM  */}
+            <div className="card bg-base-100 shadow-xl m-2">
+              <figure><img src="images/FarmingSim-2.png" alt="Farming Sim" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">Farming Sim</h2>
+                <p>With Farming Sim, you can plant any kind of plant you'd like as well as give it any fertilizer you'd like. By using <span className='btn-primary'>Python</span> and <span className='btn-primary'>Django</span>, this app demonstrates <span className='btn-primary'>CRUD</span>.</p>
+                <div className="card-actions justify-end">
+                  <a href="https://python-farming-sim.herokuapp.com/" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Go To App
+                    </button>
+                  </a>
+                  <a href="https://github.com/WangManLex/farming-sim" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Github
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          {/* FARMING SIM  */}
-          <div className="card text-center shadow-2xl">
-            <figure className="px-10 pt-10">
-              <img src="images/FarmingSim.png" alt="FarmingSim Landing Page" />
-              <img src="images/FarmingSim-2.png" alt="FarmingSim Plant List Page" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Farming Sim</h2>
-              <p>With Farming Sim, you can plant any kind of plant you'd like as well as give it any fertilizer you'd like. By using Python and Django, this app demonstrates CRUD.</p>
-              <div className="justify-center card-actions">
-                <a href="https://python-farming-sim.herokuapp.com/" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Go To App
-                  </button>
-                </a>
-                <a href="https://github.com/WangManLex/farming-sim" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Github
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* BLACKJACK */}
-          <div className="card text-center shadow-2xl">
-            <figure className="px-10 pt-10">
-              <img src="images/BlackJack.png" alt="Blackjack" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Blackjack</h2>
-              <p>Blackjack was created using JavaScript, HTML, and CSS to tackle the concepts of game logic.</p>
-              <div className="justify-center card-actions">
-                <a href="https://wangmanlex.github.io/Blackjack/" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Go To App
-                  </button>
-                </a>
-                <a href="https://github.com/WangManLex/Blackjack" target='_blank' rel='noreferrer'>
-                  <button className="btn">
-                    Github
-                  </button>
-                </a>
+            {/* BLACKJACK */}
+            <div className="card bg-base-100 shadow-xl m-2">
+              <figure><img src="images/BlackJack.png" alt="BlackJack" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">BlackJack</h2>
+                <p>Blackjack was created using <span className='btn-primary'>JavaScript</span>, <span className='btn-primary'>HTML</span>, and <span className='btn-primary'>CSS</span> to tackle the concepts of game logic.</p>
+                <div className="card-actions justify-end">
+                  <a href="https://wangmanlex.github.io/Blackjack/" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Go To App
+                    </button>
+                  </a>
+                  <a href="https://github.com/WangManLex/Blackjack" target='_blank' rel='noreferrer'>
+                    <button className="btn">
+                      Github
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
         <div className="divider w-8/12 m-auto" id='contact'></div>
         {/* -------- REACH ME ------------ */}
         <div className='flex-column text-center'>
           <p className='text-4xl m-9'>Get In Touch</p>
-          <button className="btn btn-primary mb-9">
+          <button className="btn btn-primary mb-9 mx-2">
             alexandermaniwang99@gmail.com
           </button>
         </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = (props) => {
    return (
       <>
-         <div className="navbar shadow-lg bg-black text-neutral-content">
+         <div className="navbar fixed shadow-lg bg-black text-neutral-content w-full">
             <div className="px-2 mx-2 navbar-start">
                <span className="text-lg font-bold">
                   Alexander Maniwang
                </span>
             </div>
-            <div className="hidden px-2 mx-2 navbar-end lg:flex">
+            <div className="px-2 mx-2 navbar-end lg:flex">
                <div className="flex items-stretch">
                   <a className="btn btn-ghost btn-sm rounded-btn" href='#about-me'>
                      About Me
@@ -17,9 +17,9 @@ const Navbar = (props) => {
                   <a className="btn btn-ghost btn-sm rounded-btn" href='#portfolio'>
                      Portfolio
                   </a>
-                  <a className="btn btn-ghost btn-sm rounded-btn" href='#skills'>
+                  {/* <a className="btn btn-ghost btn-sm rounded-btn" href='#skills'>
                      Skills
-                  </a>
+                  </a> */}
                   <a className="btn btn-ghost btn-sm rounded-btn" href='#contact'>
                      Contact
                   </a>
